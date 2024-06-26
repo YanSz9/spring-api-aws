35e02250fa50fdb07fc6ae2ec26c3bc9900a1f80
Yan Corrêa <yansouza009@gmail.com>
2024-06-26 17:38:44 -0300

    feat: initial commit

---
+++ src/main/java/com/correa/springawsdeploy/SpringawsdeployApplication.java
@@ -0,0 +1,13 @@
+package com.correa.springawsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringawsdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringawsdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/correa/springawsdeploy/SpringawsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.correa.springawsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringawsdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
