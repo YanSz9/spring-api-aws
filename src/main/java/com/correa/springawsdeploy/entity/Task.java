57a14aea88a36ffcec8d7e988c86b05aac25bc23
Yan Corrêa <yansouza009@gmail.com>
2024-06-26 17:55:54 -0300

    feat: create task entity

---
+++ src/main/java/com/correa/springawsdeploy/entity/Task.java
@@ -0,0 +1,32 @@
+package com.correa.springawsdeploy.entity;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
+
+import java.util.UUID;
+
+@DynamoDbBean
+public class Task {
+    private UUID taskID;
+    private String description;
+
+    @DynamoDbPartitionKey
+    public UUID getTaskID() {
+        return taskID;
+    }
+
+    public void setTaskID(UUID taskID) {
+        this.taskID = taskID;
+    }
+
+    @DynamoDbSortKey
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
